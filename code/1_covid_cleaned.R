c78c5a9f7ce7bac97e8f751a90abe201e5eab271
Seiryo Sasaki <seiryo.sasaki@emory.edu>
2026-04-16 00:52:50 -0400

    save my changes

--- code/1_covid_cleaned.R
+++ code/1_covid_cleaned.R
@@ -110,3 +110,4 @@ table(covid$covid_positive_bin, useNA = "ifany")
 # 9. EXPORT CLEANED DATA
 # ==========================================
 saveRDS(covid, file = here::here("output", "covid_cleaned.rds"))
+
