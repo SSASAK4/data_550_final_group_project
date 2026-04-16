228d394ebcbced083e4430d8edc926ea6a4209b9
Alyssa Jesse Hugo <alyssa.jesse.hugo@emory.edu>
2026-04-16 01:03:41 -0400

    Updated my code script to make table one

--- code/2_covid_tableone.R
+++ code/2_covid_tableone.R
@@ -7,6 +7,21 @@ data <- readRDS(
   file = here::here("output", "covid_cleaned.rds")
 )
 
+covid <- data %>%
+  rename(
+    sex                = SEX,
+    diabetes_bin       = DIABETES,
+    copd_bin           = COPD,
+    asthma_bin         = ASTHMA,
+    inmsupr_bin        = INMSUPR,
+    hypertension_bin   = HIPERTENSION,
+    other_disease_bin  = OTHER_DISEASE,
+    cardiovascular_bin = CARDIOVASCULAR,
+    obesity_bin        = OBESITY,
+    renal_bin          = RENAL_CHRONIC,
+    tobacco_bin        = TOBACCO
+  )
+
 bin_vars <- c("diabetes_bin", "copd_bin", "asthma_bin",
               "hypertension_bin", "inmsupr_bin", "other_disease_bin",
               "cardiovascular_bin", "obesity_bin", "renal_bin", "tobacco_bin")
@@ -14,7 +29,7 @@ bin_vars <- c("diabetes_bin", "copd_bin", "asthma_bin",
 covid <- data |>
   mutate(across(all_of(bin_vars),
                 ~ na_if(.x, 97) |> na_if(98) |> na_if(99))) |>
-  select("AGE", "sex",          
+  select("AGE", "sex",
          "diabetes_bin", "copd_bin", "asthma_bin",
          "hypertension_bin", "inmsupr_bin", "other_disease_bin",
          "cardiovascular_bin", "obesity_bin", "renal_bin", "tobacco_bin",
